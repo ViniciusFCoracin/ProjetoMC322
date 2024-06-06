1fd923222bd3811d13e6cebb1ace388eee67cceb
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-06 11:17:14 -0300

    SelectionView is now a Singleton. Created openStage and closeStage methods

--- src/GraphicInterface/Views/SelectionView.java
+++ src/GraphicInterface/Views/SelectionView.java
@@ -9,11 +9,28 @@ import javafx.stage.Stage;
 
 public class SelectionView {
 	
-	public SelectionView(Stage stage) throws IOException{
+	private Stage stage;
+	private static SelectionView instance;
+	
+	private SelectionView(Stage stage){
+		this.stage = stage;
+	}
+	
+	public static SelectionView getInstance(Stage stage){
+        if(instance == null) {
+            instance= new SelectionView(stage);
+        }
+        return instance;
+    }
+	
+	public void openStage() throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/src/FXML/selection.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        
+	}
+	
+	public void closeStage() {
+		stage.close();
 	}
 }
