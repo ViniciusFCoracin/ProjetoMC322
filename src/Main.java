7e460a1951b7bdb3233306ab303004ef99721d3d
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-07 11:31:01 -0300

    Remove unused code

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
 package src;
 
-import java.util.List;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import src.GraphicInterface.Views.SelectionView;
 
-import src.Course.Course;
-import src.Course.Discipline;
-import src.Course.Lecture;
-import src.Readers.CourseRelatedReaders.CoursesFileReader;
-import src.Readers.DisciplineRelatedReaders.DisciplinesFileReader;
-import src.Readers.SpaceRelatedReaders.SpacesFileReader;
-import src.Spaces.Space;
-import src.System.MainSystem;
-
-public class Main {
+public class Main extends Application{
 
     public static void main(String[] args) throws Exception {
-        List<Discipline> allDisciplines = DisciplinesFileReader.getInstance().readFile("src/XML/disciplines.xml");
-        List<Space> allSpaces = SpacesFileReader.getInstance().readFile("src/XML/spaces.xml");
-        List<Course> allCourses = CoursesFileReader.getInstance().readFile("src/XML/courses.xml");
-
-        List<Lecture> allLectures = MainSystem.assignSchedulesAndPlaces(allSpaces, allCourses, allDisciplines);
-
-        for (Lecture lecture : allLectures) {
-            String output = ("Lecture: " + lecture.getLectureDisciplineId() + ", Professor " + lecture.getProfessor() + "\n" +
-                             "Day: " + lecture.getLectureSchedule().getDay() + ", " + lecture.getLectureSchedule().getHourOfClass() + "\n" +
-                             "Place: " + lecture.getLectureSpace() + "\n" + "Group: " 
-                             + lecture.getLectureGroup() +"\n" + lecture.getCourseName() + "\n");
-           System.out.println(output);
-        }
+    	Application.launch(args);
     }
+    
+    @Override
+	public void start(Stage stage) throws Exception {
+		SelectionView.getInstance(stage).openStage();;
+	}
 }
