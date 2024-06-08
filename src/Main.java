2aee487a179c7aa6cbd9d2fa8c29c1a6247cbaa4
Vinicius F. Coracin <forato120504@gmail.com>
2024-06-08 18:01:15 -0300

    add javafx to project dependencies

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
 package src;
 
-import java.util.List;
-import java.util.Collections;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import src.GraphicInterface.Views.SelectionView;
 
-import src.Course.Course;
-import src.Course.Discipline;
-import src.Course.Lecture;
-import src.Course.LectureComparator;
-import src.Readers.CourseRelatedReaders.CoursesFileReader;
-import src.Readers.DisciplineRelatedReaders.DisciplinesFileReader;
-import src.Readers.SpaceRelatedReaders.SpacesFileReader;
-import src.Spaces.Space;
 import src.System.*;
 
 public class Main extends Application {
