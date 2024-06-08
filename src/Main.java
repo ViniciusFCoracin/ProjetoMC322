6031b6eb29bdeeaccdcd87f597f38bb01c0aca06
Vinicius F. Coracin <forato120504@gmail.com>
2024-06-08 17:40:35 -0300

    trying to merge

--- src/Main.java
+++ src/Main.java
@@ -21,32 +21,7 @@ public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
         Application.launch(args);
-
-        List<Discipline> allDisciplines = DisciplinesFileReader.getInstance().readFile("src/XML/disciplines.xml");
-        List<Space> allSpaces = SpacesFileReader.getInstance().readFile("src/XML/spaces.xml");
-        List<Course> allCourses = CoursesFileReader.getInstance().readFile("src/XML/courses.xml");
-
-        List<Lecture> allLectures = null;
-        while (allLectures == null){
-            try {
-                allLectures = ScheduleAllocator.createLecturesWithSchedules(allCourses, allDisciplines);
-                SpaceAllocator.assignPlaces(allSpaces, allLectures, allDisciplines);
-            }
-            catch (Error e){
-                allLectures = null;
-                for (Discipline discipline : allDisciplines)
-                    discipline.resetGroup();
-            }
-        }
-
-        Collections.sort(allLectures, new LectureComparator());
-
-        for (Lecture lecture : allLectures) {
-            String output = ("Lecture: " + lecture.getLectureDiscipline().getDisciplineId() + ", Professor " + lecture.getProfessor() + "\n" +
-                             "Day: " + lecture.getLectureSchedule().getDay() + ", " + lecture.getLectureSchedule().getHourOfClass() + "\n" +
-                             "Place: " + lecture.getLectureSpace() + "\n" + "Group: " 
-                             + lecture.getLectureGroup() +"\n" + lecture.getCourse().getCourseName() + ", " + lecture.getCourse().getDisciplineSemester(lecture.getLectureDiscipline()) + " semester\n");
-           System.out.println(output);
+        LectureSelector.startDistribution();
     }
 
     @Override
