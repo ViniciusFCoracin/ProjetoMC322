7686227905c0f1df2e347c4d726beab10a2a5fdb
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-10 16:51:21 -0300

    ComboBox test

--- src/GraphicInterface/Controllers/ScheduleController.java
+++ src/GraphicInterface/Controllers/ScheduleController.java
@@ -1,5 +1,35 @@
 package src.GraphicInterface.Controllers;
 
-public class ScheduleController {
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import src.Course.Course;
+import src.System.LectureSelector;
+
+public class ScheduleController {
+	
+	@FXML
+	private ComboBox<String> coursesComboBox;
+	
+	private List<String> allCourses;
+	private ObservableList<String> observableCoursesList;
+	
+	@FXML
+	public void initialize() {
+		loadCoursesComboBox();
+	}
+	
+	public void loadCoursesComboBox() {
+		allCourses = new ArrayList<String>();
+		for(Course course : LectureSelector.getInstance().getAllCourses()) {
+			allCourses.add(course.getCourseName());
+		}
+		
+		observableCoursesList = FXCollections.observableArrayList(allCourses);
+		coursesComboBox.setItems(observableCoursesList);
+	}
 }
