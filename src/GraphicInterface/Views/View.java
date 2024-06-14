5e2b96be9c5e21e4159638c4074af182924c3a7b
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-14 17:56:51 -0300

    Now open showStage method runs at JavaFX Application Thread

--- src/GraphicInterface/Views/View.java
+++ src/GraphicInterface/Views/View.java
@@ -2,6 +2,7 @@ package src.GraphicInterface.Views;
 
 import java.io.IOException;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,16 +10,33 @@ import javafx.stage.Stage;
 
 public abstract class View {
 	private Stage stage;
+	private Scene scene;
 	
 	public View(Stage stage) {
 		this.stage = stage;
 	}
 	
+	public Stage getStage() {
+		return stage;
+	}
+	
 	public void openStage(String path) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/src/FXML/"+path+".fxml"));
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         stage.setScene(scene);
-        stage.show();
+       
+        showStage();
+	}
+	
+	public void showStage() {
+	    Platform.runLater(() -> {
+	        try {
+	        	stage.setMaximized(true);
+	            stage.show();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    });
 	}
 	
 	public void closeStage() {
@@ -29,7 +47,8 @@ public abstract class View {
 		stage.hide();
 	}
 	
-	public void showStage() {
-		stage.show();
+	public void printStageSize() {
+		System.out.println(stage.heightProperty().get());
+		System.out.println(stage.widthProperty().get());
 	}
 }
