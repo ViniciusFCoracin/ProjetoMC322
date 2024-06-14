fd4d3414423b714aeb6a338d04a36a606bce672f
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-14 14:37:06 -0300

    Added Elective preview popUp

--- src/GraphicInterface/Controllers/ElectivesController.java
+++ src/GraphicInterface/Controllers/ElectivesController.java
@@ -1,11 +1,19 @@
 package src.GraphicInterface.Controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Popup;
 import src.Course.Lecture;
+import src.GraphicInterface.Views.ElectivesView;
 import src.Schedule.WeekDay;
 import src.System.LectureSelector;
 
@@ -38,7 +46,40 @@ public class ElectivesController {
 		
 		Label labelDisciplineId = new Label(lecture.getLectureDiscipline().getDisciplineId());
 		
+		Pane previewPane = new Pane();
+        previewPane.setStyle("-fx-background-color: lightgrey; -fx-border-color: black;");
+        previewPane.setPrefSize(200, 120);
+        
+        List<Label> previewPaneLabels = createPreviwPaneLabels(lecture);
+		
+		VBox previewPaneVBox = new VBox();
+		previewPaneVBox.getChildren().addAll(previewPaneLabels);
+		
+		previewPane.getChildren().add(previewPaneVBox);
+		
+		Popup previewPopup = new Popup();
+        previewPopup.getContent().add(previewPane);
+        
+        labelDisciplineId.setOnMouseEntered(event -> {
+            previewPopup.show(ElectivesView.getInstance(null).getStage(), event.getScreenX(), event.getScreenY() + 10);
+        });
+
+        labelDisciplineId.setOnMouseExited(event -> {
+            previewPopup.hide();
+        });
+
 		VBox vBox = (VBox) GridController.getNodeByRowColumnIndex(electivesGridPane, 1, column);
 		vBox.getChildren().addAll(labelDisciplineId);
+	}
+	
+	private List<Label> createPreviwPaneLabels(Lecture lecture) {
+		Label labelName = new Label(lecture.getLectureDiscipline().getDisciplineName());
+        Label labelProfessor = new Label(lecture.getProfessor());
+		Label labelSpace = new Label(lecture.getLectureSpace().getSpaceID());
+		Label labelCredits = new Label("Credits: " +  String.valueOf(lecture.getLectureDiscipline().getDisciplineCredits()));
+		Label labelGroup = new Label("Group: " + Character.toString(lecture.getLectureGroup()));
+		Label labelHourOfClass = new Label(lecture.getLectureSchedule().getHourOfClass().toString());
+		
+		return new ArrayList<Label>(Arrays.asList(labelName, labelProfessor, labelSpace, labelCredits, labelGroup, labelHourOfClass));
 	}
 }
