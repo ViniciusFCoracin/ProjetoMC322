67891039d061af8e66d2732d4f05aaa3d3636aba
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-17 10:16:42 -0300

    Added documentation

--- src/GraphicInterface/Views/ScheduleView.java
+++ src/GraphicInterface/Views/ScheduleView.java
@@ -1,10 +1,13 @@
 package src.GraphicInterface.Views;
 
+/**
+ * Singleton class responsible for displaying schedule stage
+ */
 public class ScheduleView extends View {
 	private static ScheduleView instance;
 	
 	private ScheduleView(){
-
+		// does nothing, but we need this to be private
 	}
 	
 	public static ScheduleView getInstance(){
