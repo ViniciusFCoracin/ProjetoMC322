31bafc852f42759db700ffa36691e2c6969e9969
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2024-06-18 10:50:55 -0300

    added style

--- src/GraphicInterface/Controllers/ScheduleController.java
+++ src/GraphicInterface/Controllers/ScheduleController.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,7 +15,10 @@ import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import src.CourseRelated.Course;
 import src.CourseRelated.LectureRelated.Lecture;
 import src.GraphicInterface.Views.ElectivesView;
@@ -31,7 +37,7 @@ public class ScheduleController {
 	private ComboBox<String> coursesComboBox, semesterComboBox;
 	
 	@FXML
-	private Label course, semester;
+	private Label course, semester, invalidCourse, invalidSemester;
 	
 	@FXML
 	private GridPane scheduleGridPane;
@@ -54,12 +60,20 @@ public class ScheduleController {
 		cleanSchedule();
 		currentCourse = coursesComboBox.getValue();
 		currentSemester = semesterComboBox.getValue();
+		invalidCourse.setText("");
+		invalidSemester.setText("");
 		
-		if(currentCourse == null || currentSemester == null) {
-			return;
+		if (currentCourse == null || currentSemester == null) {
+		    if (currentCourse == null) 
+		        invalidCourse.setText("Please choose a course");
+
+		    if (currentSemester == null) 
+		        invalidSemester.setText("Please choose a semester");
+    
+		    return;
 		} else {
 			course.setText(currentCourse);
-			semester.setText(currentSemester);
+			semester.setText("Semester: " + currentSemester);
 		}
 		
 		int currentSemesterInt = convertSemesterToNumber(currentSemester);
@@ -106,19 +120,33 @@ public class ScheduleController {
 	
 	
 	private void assignLectureToGrid(Lecture lecture) {
-		WeekDay day = lecture.getLectureSchedule().getDay();
-		HourOfClass hourOfClass = lecture.getLectureSchedule().getHourOfClass();
-		
-		int column = WeekDay.getNumericValue(day);
-		int row = HourOfClass.getNumericValue(hourOfClass);
-		
-		Label labelDisciplineId = new Label(lecture.getLectureDiscipline().getDisciplineId());
-		Label labelProfessor = new Label(lecture.getProfessor());
-		Label labelSpace = new Label(lecture.getLectureSpace().getSpaceID());
-		Label labelGroup = new Label(Character.toString(lecture.getLectureGroup()));
-		
-		VBox vBox = (VBox) getNodeByRowColumnIndex(scheduleGridPane, row, column);
-		vBox.getChildren().addAll(labelDisciplineId, labelGroup, labelProfessor, labelSpace);
+	    WeekDay day = lecture.getLectureSchedule().getDay();
+	    HourOfClass hourOfClass = lecture.getLectureSchedule().getHourOfClass();
+
+	    int column = WeekDay.getNumericValue(day);
+	    int row = HourOfClass.getNumericValue(hourOfClass);
+
+	    Label labelDisciplineId = new Label(lecture.getLectureDiscipline().getDisciplineId());
+	    Label labelProfessor = new Label(lecture.getProfessor());
+	    Label labelSpace = new Label(lecture.getLectureSpace().getSpaceID());
+	    Label labelGroup = new Label(Character.toString(lecture.getLectureGroup())); 
+	    List<Label> labels = new ArrayList<Label>(Arrays.asList(labelDisciplineId, labelGroup, labelProfessor, labelSpace));
+	    
+	    Font initialFont = new Font("Liberation Serif", 15);
+	    updateFont(initialFont, labels);
+
+	    VBox vBox = (VBox) getNodeByRowColumnIndex(scheduleGridPane, row, column);
+	    vBox.getChildren().addAll(labels);
+
+	    vBox.heightProperty().addListener((obs, oldHeight, newHeight) -> {
+	        double newSize = newHeight.doubleValue() / 5;
+	        double minFontSize = 15.0;
+	        double maxFontSize = 20.0;
+	        double adjustedFontSize = Math.max(minFontSize, Math.min(maxFontSize, newSize));
+	        
+	        Font font = new Font("Liberation Serif", adjustedFontSize);
+	        updateFont(font, labels);
+	    });
 	}
 	
 	public  static Node getNodeByRowColumnIndex(GridPane gridPane, int row, int column) {
@@ -148,5 +176,11 @@ public class ScheduleController {
 		String numericString = currentSemester.replace("°", "");
 		
 		return Integer.parseInt(numericString);
+	}
+	
+	private void updateFont (Font font, List<Label> labels) {
+		for(Label label : labels) {
+			label.setFont(font);
+		}
 	}
 }
